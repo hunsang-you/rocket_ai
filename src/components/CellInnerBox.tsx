d39dfeceaf9a58d610e5b3758733c9f09d9ac0d6
hunsang <gjstkd123@naver.com>
2025-08-24 15:51:31 +0900

    CellInnerBox 배경색 오류 수정

--- src/components/CellInnerBox.tsx
+++ src/components/CellInnerBox.tsx
@@ -10,11 +10,10 @@ export default function CellInnerBox({
   return (
     <>
       <div
-        className={`bg-[${bgColor}] rounded-[8px] w-[3rem] h-[3rem] flex items-center justify-center mx-auto ${
+        style={{ backgroundColor: bgColor }}
+        className={`rounded-[8px] w-[3rem] h-[3rem] flex items-center justify-center mx-auto ${
           border ? "border border-gray-400" : ""
-        }
-        ${bgColor == "#FDFDFB" ? "text-black" : "text-white"} 
-        `}
+        } ${bgColor === "#FDFDFB" ? "text-black" : "text-white"}`}
       >
         <div className="flex flex-col items-center leading-tight text-center">
           <span className="text-[8px]">{label}</span>
